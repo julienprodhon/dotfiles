56bccb820c1f0e3d3c445950aec150f7ef4f1158
julienprodhon <julien@prodhon.net>
2025-12-25 21:52:41 +0800

    update nvim config

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -8,6 +8,8 @@ return {
     -- Setup Mason
     require("mason").setup()
     
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    
     -- Setup mason-lspconfig
     require("mason-lspconfig").setup({
       ensure_installed = {
@@ -18,8 +20,9 @@ return {
         "dockerls",      -- Dockerfile
         "jsonls",        -- JSON
         "julials",       -- Julia
-        "pylsp",         -- Python
+        "ruff",          -- Python (linting + formatting)
         "sqlls",         -- SQL
+        "texlab",        -- LaTeX
         "yamlls",        -- YAML
       },
     })
@@ -28,22 +31,38 @@ return {
     vim.api.nvim_create_autocmd("LspAttach", {
       callback = function(ev)
         local opts = { buffer = ev.buf }
+        vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
         vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+        vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+        
+        -- Auto-format on save
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          buffer = ev.buf,
+          callback = function()
+            vim.lsp.buf.format({ async = false })
+          end,
+        })
       end,
     })
 
     -- List of servers to enable
     local servers = {
-      "bashls", "clangd", "cmake", "dockerls", 
-      "jsonls", "julials", "pylsp", "sqlls", "yamlls"
+      "bashls", "clangd", "cmake", "dockerls",
+      "jsonls", "julials", "ruff", "sqlls", "texlab", "yamlls"
     }
     
     -- Lua with special settings
     vim.lsp.config.lua_ls = {
+      capabilities = capabilities,
       settings = {
         Lua = {
           diagnostics = { globals = { "vim" } },
@@ -51,8 +70,19 @@ return {
       },
     }
     
+    -- Clangd with special settings
+    vim.lsp.config.clangd = {
+      capabilities = capabilities,
+      cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--header-insertion=iwyu",
+      },
+    }
+    
     -- Enable all servers (including lua_ls)
     table.insert(servers, 1, "lua_ls")
-    vim.lsp.enable(servers)
+    vim.lsp.enable(servers, { capabilities = capabilities })
   end,
 }
