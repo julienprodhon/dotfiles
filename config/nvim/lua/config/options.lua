4a78405e5ebd8f35418adc676eb70bae6103e316
julienprodhon <julien@prodhon.net>
2025-12-25 21:52:41 +0800

    update nvim config

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -8,4 +8,10 @@ vim.opt.relativenumber = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 
-vim.opt.clipboard = "unnamed,unnamedplus"
+vim.opt.clipboard = "unnamedplus"
+
+-- Window navigation keymaps
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
