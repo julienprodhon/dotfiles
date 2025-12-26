6124aac8333ec76c84c690d273d8425c85ba2471
julienprodhon <julien@prodhon.net>
2025-12-26 17:29:01 +0800

    update zsh

--- install-wm.sh
+++ install-wm.sh
@@ -3,36 +3,46 @@ set -e
 
 echo "=== Installing WM and Utilities ==="
 
-# Install base dependencies
 sudo pacman -Syu --needed --noconfirm base-devel git curl wget
 
-# Install yay if not present
 if ! command -v yay &>/dev/null; then
-    echo "Installing yay..."
-    TEMP=$(mktemp -d)
-    cd "$TEMP"
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
-    makepkg -si --noconfirm
-    cd ~
-    rm -rf "$TEMP"
+  echo "Installing yay..."
+  TEMP=$(mktemp -d)
+  cd "$TEMP"
+  git clone https://aur.archlinux.org/yay.git
+  cd yay
+  makepkg -si --noconfirm
+  cd ~
+  rm -rf "$TEMP"
 fi
 
-# Install WM and utilities
-yay -S --needed --noconfirm qt5-wayland sddm zsh tmux unzip npm man-pages tldr uv python-pip luarocks tree-sitter-cli ripgrep
+yay -S --needed --noconfirm qt5-wayland sddm zsh tmux npm man-pages tldr
 
-# Install oh-my-zsh
 if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
-    echo "Installing oh-my-zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  echo "Installing oh-my-zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 else
-    echo "oh-my-zsh already installed"
+  echo "oh-my-zsh already installed"
 fi
 
-# Install Dank Material Shell, Niri and Ghostty
-curl -fsSL https://install.danklinux.com | sh
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 
-yay -S -needed --noconfirm greetd-dms-greeter-git
+if [[ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]]; then
+  echo "Installing zsh-autosuggestions..."
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+else
+  echo "zsh-autosuggestions already installed"
+fi
+
+if [[ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]]; then
+  echo "Installing zsh-syntax-highlighting..."
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+else
+  echo "zsh-syntax-highlighting already installed"
+fi
+
+curl -fsSL https://install.danklinux.com | sh
+yay -S --needed --noconfirm greetd-dms-greeter-git
 dms greeter enable
 dms greeter sync
 
