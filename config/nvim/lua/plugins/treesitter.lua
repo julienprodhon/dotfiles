bfe5fb4ea1d218e0e41eaafceca09171274dfbdf
julienprodhon <julien@prodhon.net>
2025-12-27 13:08:08 +0800

    update treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,45 +1,44 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      -- Install parsers for these languages
-      ensure_installed = {
-        "bash",
-        "c",
-        "cmake",
-        "comment",
-        "cpp",
-        "csv",
-        "dockerfile",
-        "gitattributes",
-        "gitcommit",
-        "gitignore",
-        "json",
-        "julia",
-        "lua",
-        "make",
-        "vim",
-        "python",
-        "sql",
-        "yaml",
-      },
-      
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-      
-      -- Automatically install missing parsers when entering buffer
-      auto_install = true,
-      
-      highlight = {
-        enable = true,
-        -- Set to false if you have issues with highlighting
-        additional_vim_regex_highlighting = false,
-      },
-      
-      indent = {
-        enable = true,
-      },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	main = "nvim-treesitter.config",
+	opts = {
+		-- Install parsers for these languages
+		ensure_installed = {
+			"bash",
+			"c",
+			"cmake",
+			"comment",
+			"cpp",
+			"csv",
+			"dockerfile",
+			"gitattributes",
+			"gitcommit",
+			"gitignore",
+			"json",
+			"julia",
+			"lua",
+			"make",
+			"vim",
+			"python",
+			"sql",
+			"yaml",
+		},
+
+		-- Install parsers synchronously (only applied to `ensure_installed`)
+		sync_install = false,
+
+		-- Automatically install missing parsers when entering buffer
+		auto_install = true,
+
+		highlight = {
+			enable = true,
+			-- Set to false if you have issues with highlighting
+			additional_vim_regex_highlighting = false,
+		},
+
+		indent = {
+			enable = true,
+		},
+	},
 }
