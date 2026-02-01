7849aa7a8e11c21efbecf09ca5961917cdc10ac5
julienprodhon <julien@prodhon.net>
2026-02-01 19:06:54 +0800

    udpate for theming

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,18 @@
+local dankcolors_path = vim.fn.stdpath("config") .. "/lua/plugins/dankcolors.lua"
+
+if vim.fn.filereadable(dankcolors_path) == 1 then
+  return dofile(dankcolors_path)
+end
+
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  "RRethy/base16-nvim",
   priority = 1000,
   config = function()
-    require("catppuccin").setup({
-      flavour = "mocha", -- latte, frappe, macchiato, mocha
-      transparent_background = false,
-      integrations = {
-        treesitter = true,
-        telescope = true,
-        mason = true,
-        gitsigns = true,
-        -- Add more integrations as you install plugins
-      },
+    require('base16-colorscheme').setup({
+      base00 = '#282828', base01 = '#3c3836', base02 = '#504945', base03 = '#665c54',
+      base04 = '#bdae93', base05 = '#d5c4a1', base06 = '#ebdbb2', base07 = '#fbf1c7',
+      base08 = '#fb4934', base09 = '#fe8019', base0A = '#fabd2f', base0B = '#b8bb26',
+      base0C = '#8ec07c', base0D = '#83a598', base0E = '#d3869b', base0F = '#d65d0e',
     })
-    vim.cmd.colorscheme("catppuccin")
   end,
 }
